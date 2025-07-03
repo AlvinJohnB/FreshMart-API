9e4e82c3f596c2cca73a82f78c31cea6eacd8a3b
Reagan Yano Sajulga <rgnsajulga@gmail.com>
2025-07-03 12:01:03 +0000

    Update 'routes/productRoutes.js'

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,18 +6,18 @@ const { verify, verifyAdmin } = require("../middleware/auth");
 const productController = require("../controllers/productController");
 
 // GET all products
-router.get("/getAllProducts", productController.getAllProducts);
+router.get("/all", verify, verifyAdmin, productController.getAllProducts);
 
 // POST a new product
 router.post(
-  "/createProduct",
+  "/",
   verify,
   verifyAdmin,
   productController.createProduct
 );
 
 // GET all active products
-router.get("/getActiveProducts", productController.getActiveProducts);
+router.get("/active", productController.getActiveProducts);
 
 router.get("/:productId", productController.getProductById);
 
