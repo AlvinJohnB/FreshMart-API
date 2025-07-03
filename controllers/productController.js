f7771d7f704d00a235823a23965a47967990e8d4
alvinbregana <alvinbregana@gmail.com>
2025-07-03 19:47:24 +0800

    Added code for retrieve signle product, update product info, activate and archive products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,6 +1,5 @@
-const Product = require('../models/Product');
-
-
+const Product = require("../models/Product");
+const { errorHandler } = require("../middleware/errorHandler");
 
 // POST /products - Create a new product
 module.exports.createProduct = async (req, res) => {
@@ -12,18 +11,17 @@ module.exports.createProduct = async (req, res) => {
       name,
       description,
       price,
-      isActive
+      isActive,
     });
 
     // Save to database
     const savedProduct = await product.save();
     res.status(201).json(savedProduct);
   } catch (error) {
-    console.error('Error creating product:', error);
+    console.error("Error creating product:", error);
     res.status(400).json({ message: error.message });
   }
 };
-
 
 // GET /products - Retrieve all products
 module.exports.getAllProducts = async (req, res) => {
@@ -31,11 +29,10 @@ module.exports.getAllProducts = async (req, res) => {
     const products = await Product.find();
     res.status(200).json(products);
   } catch (error) {
-    console.error('Error fetching products:', error);
-    res.status(500).json({ message: 'Server error while fetching products' });
+    console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Server error while fetching products" });
   }
 };
-
 
 // GET /products/active - Retrieve all active products
 module.exports.getActiveProducts = async (req, res) => {
@@ -43,8 +40,104 @@ module.exports.getActiveProducts = async (req, res) => {
     const activeProducts = await Product.find({ isActive: true });
     res.status(200).json(activeProducts);
   } catch (error) {
-    console.error('Error fetching active products:', error);
-    res.status(500).json({ message: 'Server error while fetching active products' });
+    console.error("Error fetching active products:", error);
+    res
+      .status(500)
+      .json({ message: "Server error while fetching active products" });
   }
 };
 
+module.exports.getProductById = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json(product);
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+};
+
+module.exports.updateProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+    const updateData = req.body;
+
+    const updatedProduct = await Product.findByIdAndUpdate(
+      productId,
+      updateData,
+      { new: true }
+    );
+
+    if (!updatedProduct) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+
+    res.status(200).json({
+      message: "Product updated successfully",
+      success: true,
+    });
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+};
+
+module.exports.archiveProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    // check if product exists
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    // check if product is already archived
+    if (!product.isActive) {
+      return res.status(400).json({
+        message: "Product is already archived",
+        archivedProduct: product,
+      });
+    } else {
+      // Archive the product
+      product.isActive = false;
+      await product.save();
+      return res.status(200).json({
+        message: "Product archived successfully",
+        success: true,
+      });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+};
+
+module.exports.activateProduct = async (req, res) => {
+  try {
+    const productId = req.params.productId;
+
+    // check if product exists
+    const product = await Product.findById(productId);
+    if (!product) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    // check if product is already active
+    if (product.isActive) {
+      return res.status(400).json({
+        message: "Product is already active",
+        activateProduct: product,
+      });
+    } else {
+      // Activate the product
+      product.isActive = true;
+      await product.save();
+      return res.status(200).json({
+        message: "Product activated successfully",
+        success: true,
+      });
+    }
+  } catch (error) {
+    errorHandler(error, req, res);
+  }
+};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,18 +1,45 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 const { verify, verifyAdmin } = require("../middleware/auth");
 
-const productController = require('../controllers/productController');
+const productController = require("../controllers/productController");
 
 // GET all products
-router.get('/getAllProducts', productController.getAllProducts);
+router.get("/getAllProducts", productController.getAllProducts);
 
 // POST a new product
-router.post('/createProduct', verify, verifyAdmin, productController.createProduct);
+router.post(
+  "/createProduct",
+  verify,
+  verifyAdmin,
+  productController.createProduct
+);
 
 // GET all active products
-router.get('/getActiveProducts', productController.getActiveProducts);
+router.get("/getActiveProducts", productController.getActiveProducts);
 
+router.get("/:productId", productController.getProductById);
+
+router.patch(
+  "/:productId/update",
+  verify,
+  verifyAdmin,
+  productController.updateProduct
+);
+
+router.patch(
+  "/:productId/archive",
+  verify,
+  verifyAdmin,
+  productController.archiveProduct
+);
+
+router.patch(
+  "/:productId/activate",
+  verify,
+  verifyAdmin,
+  productController.activateProduct
+);
 
 module.exports = router;
