405b292bcb64fdc74021bd9c593a79534adb4736
alvinbregana <alvinbregana@gmail.com>
2025-07-04 19:09:13 +0800

    Exported cartRoutes

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -3,7 +3,12 @@ const router = express.Router();
 const cartController = require("../controllers/cartController");
 const { verify } = require("../middleware/auth");
 
-
 router.post("/add-to-cart", verify, cartController.addToCart);
 router.get("/get-cart", verify, cartController.getCart);
-router.patch("/update-cart-quantity", verify, cartController.updateCartQuantity);
+router.patch(
+  "/update-cart-quantity",
+  verify,
+  cartController.updateCartQuantity
+);
+
+module.exports = router;
