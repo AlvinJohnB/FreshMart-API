bd7c1c3ea4853018121adf02d17497aa2ec0cb8a
alvinbregana <alvinbregana@gmail.com>
2025-07-04 20:11:27 +0800

    Updated Products Model to satisfy data model reqs

--- models/Product.js
+++ models/Product.js
@@ -6,34 +6,25 @@ const productSchema = new mongoose.Schema({
     type: String,
     required: true,
     minlength: 4,
-    maxlength: 10
+    maxlength: 10,
   },
   description: {
     type: String,
     minlength: 4,
-    maxlength: 20
+    maxlength: 20,
   },
   price: {
-    type: mongoose.Types.Decimal128,
+    type: Number,
     required: true,
-    validate: {
-      validator: function (v) {
-        return v.toString().replace('.', '').length <= 6;
-      },
-      message: props => `${props.value} exceeds the maximum allowed 6 digits (excluding decimal point)`
-    }
   },
   isActive: {
     type: Boolean,
-    default: true
+    default: true,
   },
   createdOn: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+  },
 });
 
-// Create the model
-const Product = mongoose.model('Product', productSchema);
-
-module.exports = Product;
+module.exports = mongoose.model("Product", productSchema);
