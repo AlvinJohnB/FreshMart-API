c6b0e2fa32621b0f8a0fdc14f002a1889ad013c0
Reagan Sajulga <rgnsajulga@gmail.com>
2025-07-04 19:03:52 +0800

    s53 controller route prep 2

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,2 +1,23 @@
-const Product = require("../models/Cart");
+const Cart = require("../models/Cart");
 const { errorHandler } = require("../middleware/errorHandler");
+
+
+module.exports.addToCart = async (req, res) => {
+  return true;
+};
+
+
+module.exports.getCart = async (req, res) => {
+  try {
+    const products = await Cart.find();
+    res.status(200).json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    res.status(500).json({ message: "Server error while fetching products" });
+  }
+};
+
+
+module.exports.updateCartQuantity = async (req, res) => {
+  return true;
+};
--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 const cartController = require("../controllers/cartController");
 const { verify } = require("../middleware/auth");
+
+
+router.post("/add-to-cart", verify, cartController.addToCart);
+router.get("/get-cart", verify, cartController.getCart);
+router.patch("/update-cart-quantity", verify, cartController.updateCartQuantity);
