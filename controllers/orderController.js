5207b27660e0176ada260961bda4e37407d1b228
Reagan Yano Sajulga <rgnsajulga@gmail.com>
2025-07-08 13:03:57 +0000

    Update 'controllers/orderController.js'

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -7,8 +7,31 @@ const { checkUserExistInCart } = require("../helpers/helper");
 
 
 module.exports.checkout = async (req, res) => {
+  const userIDFromToken = req.user.id; 
   try {
-  return true;
+   const cart = await checkUserExistInCart(userIDFromToken);
+
+    console.log(cart.cartItems.length);
+    
+    if (!cart || !cart.cartItems || cart.cartItems.length === 0) {
+      return res.status(400).send({ error: "No Items to Checkout."})
+    }else{
+      console.log(cart.cartItems);
+      const newOrder = new Order({
+            userId: userIDFromToken,
+            productsOrdered: cart.cartItems,
+            totalPrice: cart.totalPrice,
+          });
+
+          await newOrder.save();
+
+          // Step 3: Clear the user's cart
+          cart.cartItems = [];
+          cart.totalPrice = 0;
+          await cart.save();
+
+          res.status(201).json({ orders: newOrder });
+    }
   } catch (error) {
     errorHandler(error, req, res);
   }
