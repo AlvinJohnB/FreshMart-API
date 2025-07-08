57e27949c1ffaf2b376408d34139b991cc466674
alvinbregana <alvinbregana@gmail.com>
2025-07-08 21:06:24 +0800

    Added member 1 code

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -5,30 +5,63 @@ const Order = require("../models/Order");
 const { errorHandler } = require("../middleware/errorHandler");
 const { checkUserExistInCart } = require("../helpers/helper");
 
-
 module.exports.checkout = async (req, res) => {
+  const { userId } = req.user.id;
   try {
-  return true;
+    // find user cart
+    const cart = await Cart.findOne({ userId });
+
+    if (!cart) {
+      return res.status(404).json({ message: "Cart not found" });
+    }
+
+    // check if cart is empty
+    if (cart.items.length === 0) {
+      return res.status(400).json({ message: "No items to checkout" });
+    }
+
+    // create order
+    const order = new Order({
+      userId,
+      productsOrdered: cart.items.map((item) => ({
+        productId: item.productId,
+        quantity: item.quantity,
+        subtotal: item.subtotal,
+      })),
+      total: cart.total,
+    });
+
+    await order.save();
+
+    // clear cart
+    await Cart.deleteOne({ userId });
+
+    return res
+      .status(201)
+      .json({ message: "Order created successfully", order });
   } catch (error) {
     errorHandler(error, req, res);
   }
 };
-
 
 module.exports.myOrders = async (req, res) => {
+  const userId = req.user.id;
   try {
-  return true;
+    // find orders for the user
+    const userOrders = await Order.find({ userId });
+    if (!userOrders || userOrders.length === 0) {
+      return res.status(404).json({ message: "No orders found for this user" });
+    }
+    return res.status(200).json({ orders: userOrders });
   } catch (error) {
     errorHandler(error, req, res);
   }
 };
-
 
 module.exports.allOrders = async (req, res) => {
   try {
-  return true;
+    return true;
   } catch (error) {
     errorHandler(error, req, res);
   }
 };
-
